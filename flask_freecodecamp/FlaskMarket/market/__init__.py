a643754a2f59e0fe6a2dc9754a96b98c828bc16d
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-08-20 17:48:27 +0545

    Initialization file for market package

---
+++ flask_freecodecamp/FlaskMarket/market/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
+# This is the key - SQLALCHEMY_DATABASE_URI; URI = Uniform Resource Identifier 
+db = SQLAlchemy(app)
+
+from market import routes
