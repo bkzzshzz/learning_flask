baa938a285ec8cd78336eb828a760fb1a7f55d6c
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-09-08 14:15:30 +0545

    Model Relationships

--- flask_freecodecamp/FlaskMarket/market/models.py
+++ flask_freecodecamp/FlaskMarket/market/models.py
@@ -1,11 +1,21 @@
 from market import db
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False) #Cuz most hashkeys are 60 chars long
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True) #MUST
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode= db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id')) #creating a relationship 'user.id' in lower case
 
     def __repr__(self): #dunder
         return f'Item {self.name}'
