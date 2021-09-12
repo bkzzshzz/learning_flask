f5edf3dcc0e623c527703bdd8e084e3c061daef4
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-09-12 11:06:19 +0545

    Added a SECRET_KEY

--- flask_freecodecamp/FlaskMarket/market/__init__.py
+++ flask_freecodecamp/FlaskMarket/market/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
-# This is the key - SQLALCHEMY_DATABASE_URI; URI = Uniform Resource Identifier 
+# This is the key - SQLALCHEMY_DATABASE_URI; URI = Uniform Resource Identifier
+app.config['SECRET_KEY'] = 'bbdf39c755e204a1ecfad191'
 db = SQLAlchemy(app)
 
 from market import routes
