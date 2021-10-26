0289b0fa8be4656b7a1d3cd7dc03be0675409b7c
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-10-26 20:59:03 +0545

    User Authenitcation part 1

--- flask_freecodecamp/FlaskMarket/market/__init__.py
+++ flask_freecodecamp/FlaskMarket/market/__init__.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
 # This is the key - SQLALCHEMY_DATABASE_URI; URI = Uniform Resource Identifier
 app.config['SECRET_KEY'] = 'bbdf39c755e204a1ecfad191'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
